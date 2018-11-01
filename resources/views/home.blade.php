e713d2895e3d964d7ba526bd002027026ec66215
8brano7 <brano87@gmail.com>
2018-11-01 16:55:01 +0100

    Opravený favicon, ktorý sa nezobrazoval na nových stránkach, kod na prihlasovanie, registráciu presunutý do home.blade, zatiaľ len zakomentované.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -255,6 +255,40 @@
     <!-- ################################################################################################ -->
     <!-- ################################################################################################ -->
     <!-- ################################################################################################ -->
+    {{-- Tu je zatial pripraveny kod na prihlasovanie, registraciu - potom sa pouzije }}
+
+
+    {{--<ul class="nav navbar-nav navbar-right">--}}
+    {{--<!-- Authentication Links -->--}}
+    {{--@guest <li><a href="{{ route('login') }}">Prihlásenie</a></li>--}}
+    {{--<li><a href="{{ route('register') }}">Registrácia</a></li>--}}
+    {{----}}
+    {{--@else--}}
+    {{--<li class="dropdown">--}}
+    {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>--}}
+    {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+    {{--</a>--}}
+
+    {{--<ul class="dropdown-menu">--}}
+    {{--<li>--}}
+    {{--<a href="{{ route('logout') }}"--}}
+    {{--onclick="event.preventDefault();--}}
+    {{--document.getElementById('logout-form').submit();">--}}
+    {{--Odhlásiť--}}
+    {{--</a>--}}
+
+    {{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
+    {{--{{ csrf_field() }}--}}
+    {{--</form>--}}
+    {{--</li>--}}
+    {{--</ul>--}}
+    {{--</li>--}}
+    {{--@endguest--}}
+    {{--</ul>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+
+    <!-- ################################################################################################ -->
 
     </body>
     </html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="images/favicon.ico" type="image/x-icon"/>
+    <link rel="icon" href="{{ URL::asset('images/favicon.ico') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::asset('layout/styles/layout.css') }}">
 </head>
@@ -79,41 +79,6 @@
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
 <!-- ################################################################################################ -->
-
-
-<!-- ################################################################################################ -->
-{{-- Tu je zatial pripraveny kod na prihlasovanie, registraciu - potom sa pouzije }}
-
-
-{{--<ul class="nav navbar-nav navbar-right">--}}
-{{--<!-- Authentication Links -->--}}
-{{--@guest <li><a href="{{ route('login') }}">Prihlásenie</a></li>--}}
-{{--<li><a href="{{ route('register') }}">Registrácia</a></li>--}}
-{{----}}
-{{--@else--}}
-{{--<li class="dropdown">--}}
-{{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>--}}
-{{--{{ Auth::user()->name }} <span class="caret"></span>--}}
-{{--</a>--}}
-
-{{--<ul class="dropdown-menu">--}}
-{{--<li>--}}
-{{--<a href="{{ route('logout') }}"--}}
-{{--onclick="event.preventDefault();--}}
-{{--document.getElementById('logout-form').submit();">--}}
-{{--Odhlásiť--}}
-{{--</a>--}}
-
-{{--<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">--}}
-{{--{{ csrf_field() }}--}}
-{{--</form>--}}
-{{--</li>--}}
-{{--</ul>--}}
-{{--</li>--}}
-{{--@endguest--}}
-{{--</ul>--}}
-{{--</div>--}}
-{{--</div>--}}
 
 
 <!-- ################################################################################################ -->
